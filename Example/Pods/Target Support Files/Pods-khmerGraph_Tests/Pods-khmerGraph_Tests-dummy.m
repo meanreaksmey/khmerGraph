da7cb8632baaccec24afecb015c4581f1be48ba8
Mean Reaksmey <meanreaksmey@2ntkh.com>
2017-08-13 21:59:57 +0700

    update

---
+++ Example/Pods/Target Support Files/Pods-khmerGraph_Tests/Pods-khmerGraph_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_khmerGraph_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_khmerGraph_Tests
+@end
---
+++ Example/Pods/Target Support Files/khmerGraph/khmerGraph-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_khmerGraph : NSObject
+@end
+@implementation PodsDummy_khmerGraph
+@end
